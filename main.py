afdefa929c4602473de1d16f848dd086c9a7c102
Mr. Robert <laughclass.co@gmail.com>
2024-04-22 01:48:39 -0500

    Displays if answer is right or wrong

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ with open("Colours.txt", "r") as file:
     colours = file.read()
     exec(colours)
 
+playable = 1
+
 # Window Parameters
 root = Tk()
 root.title("Robert's Fantastic Magical Number Game")
@@ -15,7 +17,7 @@ root.configure(bg=darkgrey)
 root.resizable(True, True)
 
 # Version
-version = "Version 0"
+version = "Version 1"
 versiontext = Label(root, text=version, fg=white, bg=darkgrey)
 versiontext.place(x=0, y=0)
 
@@ -28,34 +30,47 @@ guesslabel = Label(root, text="Take your guess", fg=blue, bg=darkgrey)
 guesslabel.grid(row=1, column=0)
 
 # Guess drop down
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 numbersdrop = StringVar()
 numbersdrop.set(numbers[0])
 numbersinput = OptionMenu(root, numbersdrop, *numbers)
 numbersinput.config(bg=darkgrey, fg=white)
 numbersinput["menu"].config(fg=white, bg=darkgrey)
 numbersinput.grid(row=2, column=0)
+guess = numbersdrop.get()
 
 # answer
-finalanswer = "0"
+finalanswer = "???"
 finalanswer_var = StringVar()
 finalanswer_var.set(finalanswer)
+
+result = ""
+result_var = StringVar()
+result_var.set(result)
 
 
 def answer():
     global finalanswer
+    global guess
+    global result_var
+    global playable
     hold = finalanswer
     while hold == finalanswer:
         finalanswer = random.randint(1, 10)
 
     finalanswer_str = str(finalanswer)
     finalanswer_var.set(finalanswer_str)
-    print(finalanswer)
+    if guess == finalanswer_str:
+        result_var.set("correct")
+    else:
+        result_var.set("incorrect")
+        playable = 0
 
 
 # Guess button
-guessbutton = Button(root, text="GUESS", padx=10, pady=10, command=answer, fg=white, bg=green)
-guessbutton.grid(row=2, column=1)
+if playable == 1:
+    guessbutton = Button(root, text="GUESS", padx=10, pady=10, command=answer, fg=white, bg=green)
+    guessbutton.grid(row=2, column=1)
 
 # Answer Display
 answerlabel = Label(root, text="Answer", fg=blue, bg=darkgrey)
@@ -63,5 +78,12 @@ answerlabel.grid(row=1, column=2)
 
 finalanswerlabel = Label(root, textvariable=finalanswer_var, fg=blue, bg=darkgrey)
 finalanswerlabel.grid(row=2, column=2)
+
+# Results
+resultlabel = Label(root, text="Result", fg=blue, bg=darkgrey)
+resultlabel.grid(row=3, column=1)
+
+resulttext = Label(root, textvariable=result_var, fg=blue, bg=darkgrey)
+resulttext.grid(row=4, column=1)
 
 root.mainloop()
