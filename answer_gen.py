9a99f96bd6853e1f4dedd2919e1e9e66e40b8e24
Mr. Robert <laughclass.co@gmail.com>
2024-04-22 00:43:32 -0500

    I forgot how to use git hub help

--- answer_gen.py
+++ answer_gen.py
@@ -1,9 +1,13 @@
 import random
 
+from tkinter import *
+
 finalanswer = 0
+finalanswer_var = 0
 
 
 def answer():
     global finalanswer
     finalanswer = random.randint(1, 10)
+    finalanswer_var.set(finalanswer)
     print(finalanswer)
